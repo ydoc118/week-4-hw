a34b7bde9c6b9b0a1a62d0379779fa1ebb5d22c1
Cody <ydoc118@gmail.com>
2020-06-18 14:36:19 -0400

    fixed questions, timer subtracts

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,40 @@
 //Assignment Code
-var startingTime = 60;
+var startingTime = 10;
 var quizQuestions = [
     {
         questionEl: "What is 4-2?",
         answerA: "1",
         answerB: "2",
         answerC: "3",
-        answerD: "4",
-        correct: answerB
+        answerD: "4"
     },
     {
-        questionEl: "What is 10-5?",
-        answerA: "1",
-        answerB: "2",
-        answerC: "4",
-        answerD: "5",
+        questionElTwo: "What is 10-5?",
+        answerATwo: "1",
+        answerBTwo: "2",
+        answerCTwo: "4",
+        answerDTwo: "5"
+    },
+    {
+        questionElThree: "What is 2+2",
+        answerAThree: "10",
+        answerBThree: "12",
+        answerCThree: "4",
+        answerDThree: "1"
+},
+ {
+      questionElFour: "What's my name?",
+        answerAFour: "Cody",
+        answerBFour: "Brett",
+        answerCFour: "Jake",
+        answerDFour: "Max"
+    },
+    {
+        questionElFive: "How old am I?",
+        answerAFive: "22",
+        answerBFive: "30",
+        answerCFive: "18",
+        answerDFive: "45"
     }
 ];
 var quizTimer = document.getElementById("timer");
@@ -22,8 +42,39 @@ var answerA = document.getElementById("firstAnswer");
 var answerB = document.getElementById("secondAnswer");
 var answerC = document.getElementById("thirdAnswer");
 var answerD = document.getElementById("fourthAnswer");
+
+var answerATwo = document.getElementById("firstAnswerTwo");
+var answerBTwo = document.getElementById("secondAnswerTwo");
+var answerCTwo = document.getElementById("thirdAnswerTwo");
+var answerDTwo = document.getElementById("fourthAnswerTwo");
+
+var answerAThree = document.getElementById("firstAnswerThree");
+var answerBThree = document.getElementById("secondAnswerThree");
+var answerCThree = document.getElementById("thirdAnswerThree");
+var answerDThree = document.getElementById("fourthAnswerThree");
+
+var answerAFour = document.getElementById("firstAnswerFour");
+var answerBFour = document.getElementById("secondAnswerFour");
+var answerCFour = document.getElementById("thirdAnswerFour");
+var answerDFour = document.getElementById("fourthAnswerFour");
+
+var answerAFive = document.getElementById("firstAnswerFive");
+var answerBFive = document.getElementById("secondAnswerFive");
+var answerCFive = document.getElementById("thirdAnswerFive");
+var answerDFive = document.getElementById("fourthAnswerFive");
+
 var questionEl = document.getElementById("questionItem");
-var answerList = document.querySelector(".list-group")
+var questionElTwo = document.getElementById("questionItemTwo");
+var questionElThree = document.getElementById("questionItemThree");
+var questionElFour = document.getElementById("questionItemFour");
+var questionElFive = document.getElementById("questionItemFive");
+
+var answerList = document.querySelector(".list-group");
+var answerListTwo = document.querySelector(".secondList");
+var answerListThree = document.querySelector(".thirdList");
+var answerListFour = document.querySelector(".fourthList");
+var answerListFive = document.querySelector(".fifthList");
+
 var lastQuestionEl = quizQuestions.length - 1;
 var currentQuestion = 0;
 
@@ -33,6 +84,10 @@ console.log(quizQuestions);
 document.getElementById("timer").textContent = "Time Left: " + startingTime;
     
 document.getElementById("quizStart").style.display = "None";
+document.getElementById("secondQuestion").style.display = "None";
+document.getElementById("thirdQuestion").style.display = "None";
+document.getElementById("fourthQuestion").style.display = "None";
+document.getElementById("fifthQuestion").style.display = "None";
 
 //Timer function
 function timerStart() {
@@ -42,6 +97,7 @@ function timerStart() {
 
         if(startingTime === 0) {
             clearInterval(timerInterval);
+            alert("Time's Up!");
         }
 
     }, 1000);
@@ -68,8 +124,14 @@ document.getElementById("startButton").onclick = function() {
 
         answerList.addEventListener("click", function(event) {
             if (event.target.matches("#secondAnswer")) {
-                console.log("correct");
+                alert("Correct!");
                 currentQuestion++
+                secondQuestion();
+            }
+            else {
+                alert("Incorrect!");
+                currentQuestion++
+                startingTime = startingTime - 5;
                 secondQuestion();
             }
         })
@@ -79,19 +141,113 @@ document.getElementById("startButton").onclick = function() {
     
 
     function secondQuestion() {
+        document.getElementById("quizStart").style.display = "none";
+        document.getElementById("secondQuestion").style.display = "inherit";
         var q = quizQuestions[currentQuestion];
 
-        questionEl.innerHTML = q.questionEl;
-        answerA.innerHTML = q.answerA;
-        answerB.innerHTML = q.answerB;
-        answerC.innerHTML = q.answerC;
-        answerD.innerHTML = q.answerD;
+        questionElTwo.innerHTML = q.questionElTwo;
+        answerATwo.innerHTML = q.answerATwo;
+        answerBTwo.innerHTML = q.answerBTwo;
+        answerCTwo.innerHTML = q.answerCTwo;
+        answerDTwo.innerHTML = q.answerDTwo;
 
-        answerList.addEventListener("click", function(event) {
-            if (event.target.matches("#fourthAnswer")) {
-                console.log("correct")
+        answerListTwo.addEventListener("click", function(event) {
+            if (event.target.matches("#fourthAnswerTwo")) {
+                alert("Correct!")
+                currentQuestion++
+                thirdQuestion();
+            }
+            else {
+                alert("Incorrect!");
+                currentQuestion++
+                startingTime = startingTime - 5;
+                ThirdQuestion();
             }
         })
+    }
+
+
+    function thirdQuestion() {
+
+        document.getElementById("secondQuestion").style.display = "none";
+        document.getElementById("thirdQuestion").style.display = "inherit";
+        var q = quizQuestions[currentQuestion];
+
+        questionElThree.innerHTML = q.questionElThree;
+        answerAThree.innerHTML = q.answerAThree;
+        answerBThree.innerHTML = q.answerBThree;
+        answerCThree.innerHTML = q.answerCThree;
+        answerDThree.innerHTML = q.answerDThree;
+
+        answerListThree.addEventListener("click", function(event) {
+            if (event.target.matches("#thirdAnswerThree")) {
+                alert("Correct!");
+                currentQuestion++
+                fourthQuestion();
+            }
+            else {
+                alert("Incorrect!");
+                currentQuestion++
+                startingTime = startingTime - 5;
+                fourthQuestion();
+            }
+        })
+        
+    }
+
+
+    function fourthQuestion() {
+        document.getElementById("thirdQuestion").style.display = "none";
+        document.getElementById("fourthQuestion").style.display = "inherit";
+        var q = quizQuestions[currentQuestion];
+
+        questionElFour.innerHTML = q.questionElFour;
+        answerAFour.innerHTML = q.answerAFour;
+        answerBFour.innerHTML = q.answerBFour;
+        answerCFour.innerHTML = q.answerCFour;
+        answerDFour.innerHTML = q.answerDFour;
+
+        answerListFour.addEventListener("click", function(event) {
+            if (event.target.matches("#firstAnswerFour")) {
+                alert("Correct!");
+                currentQuestion++
+                fifthQuestion();
+            }
+            else {
+                alert("Incorrect!");
+                currentQuestion++
+                startingTime = startingTime - 5;
+                fifthQuestion();
+            }
+        })
+        
+    }
+
+
+    function fifthQuestion() {
+        document.getElementById("fourthQuestion").style.display = "none";
+        document.getElementById("fifthQuestion").style.display = "inherit";
+        var q = quizQuestions[currentQuestion];
+
+        questionElFive.innerHTML = q.questionElFive;
+        answerAFive.innerHTML = q.answerAFive;
+        answerBFive.innerHTML = q.answerBFive;
+        answerCFive.innerHTML = q.answerCFive;
+        answerDFive.innerHTML = q.answerDFive;
+
+        answerListFive.addEventListener("click", function(event) {
+            if (event.target.matches("#secondAnswerFive")) {
+                alert("Correct!");
+            }
+            else {
+                alert("Incorrect!");
+            }
+        })
+        
+    }
+
+    function quizEnd() {
+
     }
 
     
